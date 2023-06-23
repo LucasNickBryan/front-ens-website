322b2876cca129d2e91d463503af06195b2288ce
LucasNickBryan <rlucasnickbryan@gmail.com>
2023-06-23 13:41:03 +0300

    feat: dashboard integration and installing tailwindcss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RoutesConfig } from './config/routes';
 
+import 'bootstrap/dist/js/bootstrap'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
