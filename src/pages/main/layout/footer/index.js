9745fdacdc089b73c97ae15138c9698a127f981c
LucasNickBryan <rlucasnickbryan@gmail.com>
2023-07-27 17:31:37 +0300

    feat: footer

--- src/pages/main/layout/footer/index.js
+++ src/pages/main/layout/footer/index.js
@@ -1,29 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./styles.scss";
-import "w3-css/w3.css";
 
 export default function MainFooter() {
   return (
-    <div className="FOOTER_STYLES">
-      <section className="has-bg-img py-0">
+    <footer className="FOOTER_STYLES">
+      <section className="py-0">
         <div
-          className="container"
-          style={{ color: "black", fontWeight: "bolder" }}
+          className="!px-24"
         >
           <div
             className="footer"
-            style={{ color: "black", fontWeight: "bolder" }}
+            style={{ fontWeight: "bolder" }}
           >
             <div className="footer-lists">
               <ul className="list">
                 <li className="list-head">
-                  <h6 className="font-weight-bold">ABOUT US</h6>
+                  <h6 className="font-weight-bold">A PROPOS</h6>
                 </li>
                 <li className="list-body">
                   <NavLink className="logo">
                     <img src="assets/imgs/logo.png" alt="" />
-                    <h6>Creative Studio</h6>
+                    <h6>AEENS Fianarantsoa</h6>
                   </NavLink>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -35,67 +33,25 @@ export default function MainFooter() {
                     <script>document.write(new Date().getFullYear())</script>{" "}
                     &copy;{" "}
                     <a
-                      className="d-inline text-primary"
+                      className="d-inline !text-redcolor"
                       href="http://www.devcrud.com"
                     >
-                      DevCRUD
+                      Tout droit réservé
                     </a>
                   </p>
                 </li>
               </ul>
+
               <ul className="list">
                 <li className="list-head">
-                  <h6 className="font-weight-bold">USEFUL LINKS</h6>
+                  <h6 className="font-weight-bold">LIENS UTILS</h6>
                 </li>
                 <li className="list-body">
                   <div className="row">
                     <div className="col">
-                      <a href="#about">About</a>
-                      <a href="#service">Service</a>
-                      <a href="#portfolio">Portfolio</a>
-                      <a href="#testmonail">Testimonial</a>
+                      <a href="#about">facebook</a>
+                      <a href="#service">website</a>
                     </div>
-                    <div className="col">
-                      <a href="#team">Team</a>
-                      <a href="#blog">Blog</a>
-                      <a href="#dsd">Faq</a>
-                      <a href="#">Privacy Policy</a>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-              <ul className="list">
-                <li className="list-head">
-                  <h6 className="font-weight-bold">CONTACT INFO</h6>
-                </li>
-                <li className="list-body">
-                  <p>Contact us and we'll get back to you within 24 hours.</p>
-                  <p>
-                    <i className="ti-location-pin"></i> 12345 Fake ST NoWhere AB
-                    Country
-                  </p>
-                  <p>
-                    <i className="ti-email"></i> info@website.com
-                  </p>
-                  <div className="social-links">
-                    <a href="javascript:void(0)" className="link">
-                      <i className="ti-facebook"></i>
-                    </a>
-                    <a href="javascript:void(0)" className="link">
-                      <i className="ti-twitter-alt"></i>
-                    </a>
-                    <a href="javascript:void(0)" className="link">
-                      <i className="ti-google"></i>
-                    </a>
-                    <a href="javascript:void(0)" className="link">
-                      <i className="ti-pinterest-alt"></i>
-                    </a>
-                    <a href="javascript:void(0)" className="link">
-                      <i className="ti-instagram"></i>
-                    </a>
-                    <a href="javascript:void(0)" className="link">
-                      <i className="ti-rss"></i>
-                    </a>
                   </div>
                 </li>
               </ul>
@@ -103,8 +59,6 @@ export default function MainFooter() {
           </div>
         </div>
       </section>
-    </div>
+    </footer>
   );
 }
-
-// export default Index;
